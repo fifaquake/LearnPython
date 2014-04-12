0e37d056bababc2ec0bada97010af4cb50c61dcb
fifaquake <lipengweb@gmail.com>
2014-04-12 20:52:39 +0800

    Add two samples:collections and PriorityQueue

---
+++ LearnPythonFromVS/collections.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+q = deque(maxlen = 3)
+q.append(1)
+q.append(2)
+q.append(3)
+
+q.append(4)
+print(q)
+
+q.append(5)
+print(q)
+
+import heapq
+
+nums = [1,8,2,23,7,-4,18,23,42,37,2]
+print(heapq.nlargest(3, nums))
+print(heapq.nsmallest(3, nums))
+print(nums)
+
+portfolio = [{'name':'IBM', 'shares': 100, 'price':91.1},
+    {'name':'AAPL', 'shares':50, 'price':543.22},
+    {'name':'FB', 'shares':200, 'price':21.09},
+    {'name':'HPQ', 'shares':35, 'price':31.75},
+    {'name':'YAOO', 'shares':45, 'price':16.35},
+    {'name':'ACME', 'shares':75, 'price':115.65}]
+
+cheap = heapq.nsmallest(3, portfolio, key = lambda s : s['price'])
+expensive = heapq.nlargest(3, portfolio, key = lambda s: s['price'])
+print(cheap)
+print(expensive)
+
+heap = list(nums)
+heapq.heapify(heap)
+print(heap)
